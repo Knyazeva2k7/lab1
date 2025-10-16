5350b67790ae1aee7961ec3a6f3a51ef779e7605
Diana <knyazevaofficial@gmail.com>
2025-10-16 20:24:29 +0700

    Add files via upload

--- laba_4.cpp
+++ laba_4.cpp
@@ -48,15 +48,15 @@ int main() {
         cout << "\nчисло " << vec[i] << " повторяется " << count1 << " раз" << endl;
     }
 
-    //2 zadanie 
+    //2 zadanie переделай через динамический массив
     //1
     int n2;
     cin >> n2;
-    vector<double> vec2;
+    int* vec2 = new int [n2];
     for (int i = 0; i < n2; i++) {
         double number;
         cin >> number;
-        vec2.push_back(number);
+        vec2[i] = number;
     }
     int minModule = abs(vec2[0]);
     for (int i = 0; i < n2; i++) {
@@ -64,10 +64,10 @@ int main() {
             minModule = abs(vec2[i]);
     }
 
-    auto indMin = find(vec2.begin(), vec2.end(), minModule);
+    auto indMin = find(vec2, vec2+n2, minModule);
     //auto определяет тип переменной на основе ее инициализации
-    if (indMin != vec2.end()) {
-        int index = distance(vec2.begin(), indMin) + 1;
+    if (indMin != vec2+n2) {
+        int index = distance(vec2, indMin) + 1;
         //distance определяет "расстояние" от начала вектора
         // до нужного числа
         cout << "Номер минимального по модулю элемента массива "
@@ -88,11 +88,13 @@ int main() {
     //3
     for (int i = 0; i < n2; i++) {
         int nol = 0;
-        if (abs(vec2[i])<=2) {
-            vec2.erase(vec2.begin() + i);
-            vec2.push_back(nol);
+        if (abs(vec2[i]) <= 2) {
+            vec2[i] = nol;
         }
+        else
+            vec2[i] = abs(vec2[i]);
     }
+    sort(vec2, vec2 + n2, greater<double>());
     for (int i = 0; i < n2; i++) {
         cout << vec2[i] << " ";
     }
